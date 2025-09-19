ca9ef5337a50571d4007f57db2334b4811b22628
Charles Barilleaux <chbarr@bandman.local>
2025-09-19 10:09:06 -0400

    Moving on to import stuff and make a new app

--- Package.swift
+++ Package.swift
@@ -3,12 +3,20 @@
 
 import PackageDescription
 
+//copy-and-pasted from tutorial
 let package = Package(
     name: "MyCLI",
+    dependencies: [
+      .package(url: "https://github.com/apple/example-package-figlet", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MyCLI"),
+            name: "MyCLI",
+            dependencies: [
+                .product(name: "Figlet", package: "example-package-figlet"),
+            ],
+            path: "Sources"),
     ]
 )
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This is a pivot from learning Rust to learning Swift. I'm doing the [initial myCLI tutorial](https://www.swift.org/getting-started/cli-swiftpm/), to understand the development environment. 
 
+Setting up the project:
 ```
 $ mkdir MyCLI
 $ cd MyCLI
